68184f2a9cabb47cd655ec5253be3bc33f6c8b08
SQACraft <SQACraft@gmail.com>
2018-05-31 21:18:46 +0300

    Bubble sort vs Arrays.sort()

--- src/test/java/polygon/BubbleSort.java
+++ src/test/java/polygon/BubbleSort.java
@@ -6,13 +6,17 @@ public class BubbleSort {
 
     public static void main(String[] args) {
 
-        int m[] = new int[] {5, 2, 3, 1, 4};
+        int m[] = new int[1000] ;
 
-        int i = 0;
+        for (int i=0; i< m.length;i++){
+            m[i] = (int) (Math.random()*100);
+        }
 
+        // **************Сортировка пузырьком - БЕЗ Arrays.sort() **************
+        long t0 = System.currentTimeMillis();
         for (int k = 0; k < m.length; k++) {
 
-            i=0;
+            int i=0;
             while (i < m.length -1) {
                 if (m[i] > m[i+1]) {
                     int q;
@@ -24,5 +28,20 @@ public class BubbleSort {
                 System.out.println(Arrays.toString(m));
             }
         }
+
+        long t1 = System.currentTimeMillis();
+        System.out.println(Arrays.toString(m));
+
+        System.out.println((t1-t0)+ " ms без Arrays.sort()");
+
+        // **************Сортировка С Arrays.sort() *******************
+        for (int i=0; i<m.length; i++){
+            m[i] = (int) (Math.random()*100);
+        }
+        t0 = System.currentTimeMillis();
+        Arrays.sort(m);
+        t1 = System.currentTimeMillis();
+        System.out.println((t1-t0) + " ms  с Arrays.sort()");
+
     }
 }
