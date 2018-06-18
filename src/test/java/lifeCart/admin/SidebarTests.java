30b4818b89ba4b8fc43cec8a5958b7a0ae3dcd4e
SQACraft <SQACraft@gmail.com>
2018-06-18 17:15:41 +0300

    lifeCart:admin area - fixtures and login

---
+++ src/test/java/lifeCart/admin/SidebarTests.java
@@ -0,0 +1,17 @@
+package lifeCart.admin;
+
+import org.testng.annotations.Test;
+
+public class SidebarTests extends TestBase {
+
+    /**
+     * Проверки открытия разделов сайдбара:
+     * - открытие раздела / подраздела
+     * - проверка соответствия заголовка ожидаемому
+     */
+
+    @Test(priority = 1)
+    public void checkAppearance() {
+
+    }
+}
