74dcf5645207fc4078f3faef9f6376508a6b88bb
SQACraft <SQACraft@gmail.com>
2018-06-20 23:52:07 +0300

    Locators for a combobox

--- src/test/java/lifeCart/business/locators/Drill.java
+++ src/test/java/lifeCart/business/locators/Drill.java
@@ -10,7 +10,8 @@ public class Drill extends TestBase {
      * 1. Локатор для поиска  всех блоков (li) с информацией о товарах
      * (каждому товару соответствует свой блок)
      */
-    @Test (priority = 1)
+
+    @Test(priority = 1)
     public void TestOne() {
 
         By locator;
@@ -36,7 +37,8 @@ public class Drill extends TestBase {
      * 2. Локатор для поиска  всех ссылок (a) на страницы товаров
      * в основной части страницы  (не считая боковых блоков)
      */
-    @Test (priority = 2)
+
+    @Test(priority = 2)
     public void TestTwo() {
 
         // Блок Most Popular
@@ -59,7 +61,8 @@ public class Drill extends TestBase {
     /**
      * 3. Локатор для поиска  ссылки на Privacy Policy в нижней части страницы
      */
-    @Test (priority = 3)
+
+    @Test(priority = 3)
     public void TestThree() {
 
         click(By.cssSelector("td.information [href$='policy-i-3']"));    // клик по ссылке
@@ -67,15 +70,39 @@ public class Drill extends TestBase {
                 "//h1[contains(text(), 'Privacy Policy')]")));               // проверка заголовка
     }
 
-/**
-      4. Подберите локатор для поиска всех элементов верхнего меню,
-      находящихся на верхнем уровне (без элементов вложенных выпадающих меню)
-*/
-@Test (priority = 4)
+    /**
+     * 4. Подберите локатор для поиска всех элементов верхнего меню,
+     * находящихся на верхнем уровне (без элементов вложенных выпадающих меню)
+     */
+
+    @Test(priority = 4)
     public void TestFour() {
 
-    click(By.cssSelector("li.category-1 [href$='ducks-c-1/']"));    // клик по ссылке
-    Assert.assertTrue(isOneElementPresent(By.xpath(
-            "//h1[contains(text(), 'Rubber Ducks')]")));               // проверка заголовка
+        click(By.cssSelector("li.category-1 [href$='ducks-c-1/']"));    // клик по ссылке
+        Assert.assertTrue(isOneElementPresent(By.xpath(
+                "//h1[contains(text(), 'Rubber Ducks')]")));               // проверка заголовка
     }
+
+    /**
+     * 5. Локатор для поиска на странице /create_account
+     * элемента с текстом United States из выпадающего списка стран
+     */
+
+    @Test(priority = 5)
+    public void TestFive() {
+
+        click(By.cssSelector("td.account [href$='create_account']"));   // клик по ссылке
+        Assert.assertTrue(isOneElementPresent(By.xpath(
+                "//h1[contains(text(), 'Create Account')]")));                // проверка заголовка
+
+        click(By.cssSelector("span.select2-selection__arrow"));          // раскрыли комбобокс
+
+        wd.findElement(By.cssSelector("input.select2-search__field"))
+                .sendKeys("United States");                                                 // ввели наименование страны в комбобоксе
+
+        wd.findElement(By.cssSelector("span.select2-selection.select2-selection--single"))
+                .sendKeys("\n");                                                               // засабмитили выбор в комбобоксе по Enter
+    }
+
+
 }
