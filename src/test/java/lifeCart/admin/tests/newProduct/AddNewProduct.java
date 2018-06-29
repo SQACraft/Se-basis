00528a58b3994165b53f582523c6fd20de7dec9e
SQACraft <SQACraft@gmail.com>
2018-06-29 19:22:32 +0300

    AddNewProduct

--- src/test/java/lifeCart/admin/tests/newProduct/AddNewProduct.java
+++ src/test/java/lifeCart/admin/tests/newProduct/AddNewProduct.java
@@ -3,6 +3,8 @@ package lifeCart.admin.tests.newProduct;
 import lifeCart.admin.appManager.ToolBox;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class AddNewProduct extends ToolBox {
@@ -15,9 +17,27 @@ public class AddNewProduct extends ToolBox {
         click(By.cssSelector("a.button[href$=edit_product]")); // кнопка добавления продукта
         validateByOuterText(By.cssSelector("#content h1")," Add New Product");  // Валидация заголовка
 
-        click(By.cssSelector("input[type=radio][value='1']"));    // радиокнопка Enabled  (value=1)
+        By locator = By.cssSelector("[type=radio][value='1']");
+        click(locator);    // радиокнопка Enabled  (value=1)
+         wd.findElement(locator).sendKeys(Keys.TAB);
 
-        wd.findElement(By.cssSelector("input[name=code]"))
-                .sendKeys("Lucky Duck" + Keys.ENTER) ;
-    }
-}
+        wd.findElement(By.cssSelector("input[name='name[en]']"))    // Name
+                .sendKeys("Lucky Duck" + Keys.TAB) ;
+
+        wd.findElement(By.cssSelector("input[name=code]"))           // Code
+                .sendKeys("Lucky Duck" + Keys.TAB) ;
+
+        setCheckboxState(By.cssSelector("[type=checkbox][data-name='Rubber Ducks'"), "toSelect") ; //выбираем чекбокс категории
+        setCheckboxState(By.cssSelector("[type=checkbox][data-name='Subcategory'"), "toSelect") ;  //выбираем чекбокс подкатегории
+
+        WebElement selectElement = wd.findElement(By.cssSelector("select[name=default_category_id]"));    // cписок выбора дефолтной категории
+        Select select = new Select(selectElement);
+        select.selectByVisibleText("Rubber Ducks");        // выбираем категорию
+        selectElement.sendKeys(Keys.TAB);
+
+
+
+
+        
+        }
+        }
