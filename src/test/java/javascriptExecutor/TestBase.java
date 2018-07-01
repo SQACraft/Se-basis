0c3372c2714d04f931926884cab34511263c7a08
SQACraft <SQACraft@gmail.com>
2018-07-01 23:31:42 +0300

    More javascriptExecutor

--- src/test/java/javascriptExecutor/TestBase.java
+++ src/test/java/javascriptExecutor/TestBase.java
@@ -1,11 +1,13 @@
 package javascriptExecutor;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 import java.util.concurrent.TimeUnit;
 
--- src/test/java/javascriptExecutor/Tests.java
+++ src/test/java/javascriptExecutor/Tests.java
@@ -1,19 +1,38 @@
 package javascriptExecutor;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class Tests extends javascriptExecutor.ToolBox {
 
-
     @Test
     void documentRootLevelTest() {   //Executing JavaScript at the document root level
 
-        String strJavaScript = "document.getElementsByName('language_java')[0].click();";
         JavascriptExecutor js = (JavascriptExecutor) wd;
+        String strJavaScript = "document.getElementsByName('language_java')[0].click();";
         js.executeScript(strJavaScript);
     }
 
+    @Test
+    void elementLevelTest() {   //Executing JavaScript at the document root level
+
+        JavascriptExecutor js = (JavascriptExecutor) wd;
+        WebElement firstName = wd.findElement(By.cssSelector("#firstname"));
+        js.executeScript("arguments[0].value='Oleg';", firstName);
+    }
+
+    @Test
+    void elementClick() {   //Two ways of clicking element
+
+        JavascriptExecutor js = (JavascriptExecutor) wd;
+
+        js.executeScript("document.getElementById('firstname').click();");  //1
+
+        WebElement firstName = wd.findElement(By.cssSelector("#firstname"));  //2
+        js.executeScript("arguments[0].click();", firstName);
+    }
 
 }
 
