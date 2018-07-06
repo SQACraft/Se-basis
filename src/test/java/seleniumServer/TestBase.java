369a3cf2f6150a455d84d0462304a1b631a1789f
SQACraft <SQACraft@gmail.com>
2018-07-07 00:27:51 +0300

    Selenium Server on Ubuntu is up

--- src/test/java/seleniumServer/TestBase.java
+++ src/test/java/seleniumServer/TestBase.java
@@ -23,7 +23,7 @@ public class TestBase {
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-fullscreen");
-        wd = new RemoteWebDriver(new URL("http://10.0.2.15:4444/wd/hub"), DesiredCapabilities.chrome());
+        wd = new RemoteWebDriver(new URL("http://192.168.56.107:4444/wd/hub"), DesiredCapabilities.chrome());
 
         wait = new WebDriverWait(wd, 10);
         wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); //  неявное (Implicit) ожидание
--- src/test/java/seleniumServer/Tests.java
+++ src/test/java/seleniumServer/Tests.java
@@ -1,11 +1,18 @@
 package seleniumServer;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class Tests extends ToolBox {
 
     @Test
     void testTemplate() {
+
+
+
+            wd.get("https://bash.im/");
+            wd.findElement(By.name("text")).sendKeys("котик");
+            wd.findElement(By.xpath(".//*[@id='search']/button")).click();
 
 
     }
