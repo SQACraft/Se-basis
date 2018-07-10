c34c31e3db4e36324940eb54737f7d9714431cac
SQACraft <SQACraft@gmail.com>
2018-07-10 22:32:12 +0300

    Let's call it a night.

--- src/test/java/browserMobProxy/BrowserMobProxy.java
+++ src/test/java/browserMobProxy/BrowserMobProxy.java
@@ -1,30 +1,23 @@
 package browserMobProxy;
 
 import net.lightbody.bmp.core.har.Har;
-import org.openqa.selenium.By;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class BrowserMobProxy extends TestBase {
 
     /**
-     *  Тест прокси
+     * Тест прокси
      */
 
     @Test
     void checkProxy() {
 
         proxy.newHar();
-        wd.get("https://selenium2.ru");
-/*
-        String colour = wd.findElement(By.cssSelector("div.page-header [itemprop=headline]"))
-                .getCssValue("color");
 
-        Assert.assertEquals(colour, "rgba(214, 70, 63, 1)");        // валидация значения в формате RGBa
-*/
+        wd.get("https://selenium2.ru");
 
         Har har = proxy.endHar();
-        har.getLog().getEntries().forEach(l -> System.out.println(l.getResponse().getStatus() + ":" + l.getRequest().getUrl()) );
+        har.getLog().getEntries().forEach(l -> System.out.println(l.getResponse().getStatus() + ":" + l.getRequest().getUrl()));
     }
 
 
